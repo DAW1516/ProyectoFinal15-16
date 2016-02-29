6397e72b1b498fa78c113315c2fac58fcc40b880
Mikel Ereño <MEreno1995@hotmail.com>
2016-02-29 12:29:05 +0100

    Mover getParteByFecha

--- Modelo/BD/TrabajadorBD.php
+++ Modelo/BD/TrabajadorBD.php
@@ -33,4 +33,16 @@ abstract class TrabajadorBD extends GenericoBD{
         return $trabajador;
     }
 
+    public function getTareasParteByFecha(){
+
+        $diaSemana = date("N");
+        $fechaSemana = date("d/m/Y",strtotime("-$diaSemana day"));
+
+        if(is_null($this->tareasParte)){
+            $this->tareasParte = ParteProduccionTareaBD::getTareasByParteAndFecha($this,$fechaSemana);
+        }
+
+        return $this->tareasParte;
+    }
+
 }
