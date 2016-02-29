195c858278d0300e50a442d46971de70e5984f2f
Ruben Polo <polouters@gmail.com>
2016-02-29 12:43:43 +0100

    Primeras vistas codificadas

---
+++ Vista/Produccion/CabeceraProduccion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Produccion</title>
+</head>
+<body>
+<div class="contenedor">
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ruben
+ * Date: 29/2/16
+ * Time: 12:28
+ */ ?>
