b996123f92b106cd0b6a0e6db0faf485290a71db
Ruben Polo <polouters@gmail.com>
2016-03-02 13:07:27 +0100

    tipotarea

--- Controlador/Produccion/Controlador.php
+++ Controlador/Produccion/Controlador.php
@@ -7,14 +7,57 @@ namespace Controlador\Produccion;
  * Time: 12:03
  */
 
-use Modelo\BD\TareaBD;
+use Modelo\BD;
+use Modelo\Base;
 
 require_once __DIR__.'/../../Modelo/BD/RequiresBD.php';
+
+
+
+/**
+ *
+ *
+ */
 class Controlador
 {
-    public static function getAllTareas(){
-        return TareaBD::getAll();
+    /**
+     *Este metodo recibe un Array de objetos Tareas, para crear el Select de ParteProduccicon
+     */
+    public static function getAllTipoTarea(){
+        return Base\TipoTareaBD::getAll();
     }
+    /**
+     *En este metodo generamos un nuevo parte y lo ponemos con el estado en abierto
+     *
+     */
+    public static function abrirParte($datos){
+
+        $parte = new Base\ParteProduccion(null,"abierto",$datos['fecha'],null,null,null,null,new Base\Produccion($datos['dni']));
+
+    }
+    /**
+     *Este metodo puede ser utilizado para actualizar cualquier informacion del parte
+     *
+     */
+    public static function actualizarParte(){
+
+    }
+    /**
+     *Este metodo terimina de completar el parte y cambia el estado del parte a cerrado
+     *una vez cerrado el productor no lo puede reabrir
+     */
+    public static function cerrarParte(){
+
+
+    }
+    /**
+     *Este metodo añade una tarea al parte creando un objeto parteproduccionTarea
+     *
+     */
+    public static function añadirTarea($parte){
+
+    }
+
 
 }
 
