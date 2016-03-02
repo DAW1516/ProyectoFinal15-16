7ded6935be12028f68e938979b78a3409b1f29a7
Mikel Ereño <MEreno1995@hotmail.com>
2016-03-02 09:11:58 +0100

    Añadir nif a la tabla empresa, añadir foto a trabajador y cambiar relaciones

--- Index.php
+++ Index.php
@@ -2,19 +2,17 @@
 <?php
 require_once __DIR__ .'/Modelo/BD/EstadoBD.php';
 
-$respuesta=estadoBD::select_estado_by_id("1");
-var_dump($respuesta);
-=======
+
 /*
 	Es la clase que hara la funcion de router de Vistas principal.
 	Comprobara la variable de sesion y si esta vacia nos direccionara al Login, si ya tiene un usuario guardado nos llevara a la vista principal de ese usuario.
 */
 
-<?php
+
 
 require_once __DIR__.'/Vista/Calendario/CalendarioViews.php';
 
 CalendarioViews::login();
->>>>>>> 71d46c79aa49fb27e8d51cc5d4930c7d572374e1
+
 
 ?>
