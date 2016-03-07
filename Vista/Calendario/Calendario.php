d79f7da60272278cf46134a4f7ee1d4db2198fcc
Bengoak <martinbengoacasado@gmail.com>
2016-03-07 12:28:41 +0100

    Cambio de contraseñas

--- Vista/Calendario/Calendario.php
+++ Vista/Calendario/Calendario.php
@@ -13,36 +13,50 @@ require_once __DIR__.'/../Administracion/CalendarioViews.php';
 require_once __DIR__.'/../../Modelo/Base/GerenciaClass.php';
 require_once __DIR__.'/../Gerencia/CalendarioViews.php';
 
-require_once __DIR__.'/../../Modelo/Base/LoginClass.php';
+require_once __DIR__.'/../../Modelo/Base/LogisticaClass.php';
 require_once __DIR__.'/../Logistica/CalendarioViews.php';
 
 require_once __DIR__.'/../../Modelo/Base/ProduccionClass.php';
 require_once __DIR__.'/../Produccion/CalendarioViews.php';
 
+require_once __DIR__.'/../../Modelo/Base/LoginClass.php';
+require_once __DIR__.'/../Login/LoginViews.php';
+
+
+
+$login = unserialize($_SESSION['login']);
 
 $trabajador = unserialize($_SESSION['trabajador']);
+
+$trabajorPasswordm5 = md5($trabajador->getDni());
 
 $perfil = get_class($trabajador);
 
 $perfil = substr($perfil,12);
 
-
-switch($perfil){
-    case "Administracion":
-        \Vista\Administracion\CalendarioViews::generarcalendario();
-        break;
-    case "Gerencia":
-        \Vista\Gerencia\CalendarioViews::generarcalendario();
-        break;
-    case "Logistica":
-        \Vista\Logistica\CalendarioViews::generarcalendario();
-        break;
-    case "Produccion":
-        \Vista\Produccion\CalendarioViews::generarcalendario();
-        break;
-    default:
-        header("Location: ".\Vista\Plantilla\Views::getUrlRaiz()."/Vista/Login/Login.php");
-        break;
+if ($login->getPassword() == $trabajorPasswordm5){
+    \Vista\Login\LoginViews::changePassword();
 }
+else {
+    switch($perfil){
+        case "Administracion":
+            \Vista\Administracion\CalendarioViews::generarcalendario();
+            break;
+        case "Gerencia":
+            \Vista\Gerencia\CalendarioViews::generarcalendario();
+            break;
+        case "Logistica":
+            \Vista\Logistica\CalendarioViews::generarcalendario();
+            break;
+        case "Produccion":
+            \Vista\Produccion\CalendarioViews::generarcalendario();
+            break;
+        default:
+            header("Location: ".\Vista\Plantilla\Views::getUrlRaiz()."/Vista/Login/Login.php");
+            break;
+    }
+}
+
+
 
 
